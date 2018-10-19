b786ebe268b732987cb09c5b3c13f8e2fc364fda
Jeff Bennett <jeff@sapienti7.com>
2018-10-19 15:49:30 +0100

    Working implementation

--- public/controller.js
+++ public/controller.js
@@ -1,15 +1,50 @@
-$(init);
-
-
-const init = () => {
+$(() => {
     buildBoard();
     attachListeners();
-}
+});
 
 const buildBoard = () => {
+    let $board = $('#boardArea');
+    let content = '<table class="boardTable">';
+    let array = [];
+    for(let i = 1; i <= 36; i++){
+        array.push(i);
+    }
+    array = fisherYatesSuffle(array);
+    for( let i = 0; i < 36; i++){
+        if(i % 6 === 0){
+            if(i > 0){
+                content += '</tr>';
+            }
+            content += '<tr>';
+        }
+        content += '<td class="boardCell">'+
+                        '<div class="cellLabel"><span class="cellLabelText">'+array[i]+'</span></div>'+
+                    '</td>';
+    }
+    content += '</table>';
 
+    $board.append($(content));
 }
 
 const attachListeners = () => {
 
+}
+
+const fisherYatesSuffle = array => {
+    let m = array.length, t, i;
+
+    // While there remain elements to shuffle…
+    while (m) {
+
+        // Pick a remaining element…
+        i = Math.floor(Math.random() * m--);
+
+        // And swap it with the current element.
+        t = array[m];
+        array[m] = array[i];
+        array[i] = t;
+    }
+
+    return array;
 }
